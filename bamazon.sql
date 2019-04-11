970cc501228f6bebca1e37bb3043a850e82aeb2f
Holly <snowflake@Hollys-MacBook-Pro.local>
2019-04-11 09:46:21 -0400

    completed commit

---
+++ bamazon.sql
@@ -0,0 +1,25 @@
+
+-- DROP DATABASE bamazonDB;
+
+USE bamazonDB;
+
+-- CREATE TABLE products (
+--   item_id INTEGER(10) NOT NULL AUTO_INCREMENT,
+--   product_name VARCHAR(255) NOT NULL,
+--   department_name VARCHAR(100) NOT NULL,
+--   price DOUBLE(8,2),
+--   stock_quantity INTEGER(10) NOT NULL,
+--   PRIMARY KEY (item_id)
+-- );
+
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES("pen", "stationary", 1.50, 100),
+-- ("paper", "stationary", 6.50, 50),
+-- ("Star Wars Blu Ray set", "movies", 19.99, 300),
+-- ("Detective Pikachu", "movies", 29.99, 73),
+-- ("Zelda Breath of the Wild", "video game", 79.99, 12),
+-- ("Mario Kart 8", "video games", 79.99, 9),
+-- ("Princess Jellyfish Volume 1", "books", 16.99, 32),
+-- ("Nana Volume 12", "books", 12.99, 53),
+-- ("iPhone Cable", "electronics", 9.99, 3),
+-- ("USB charger", "electronics", 18.99, 7);
---
+++ bamazonCustomer.js
@@ -0,0 +1,102 @@
+//Bamazon Customer 
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require('cli-table');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  user: "root",
+
+  password: "root",
+  database: "bamazonDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  showStore();
+});
+
+let stock = 0;
+let updatedQuantity = 0;
+
+function showStore() {
+  var table = new Table({
+    head: ['Item ID', 'Product Name', 'Price']
+  });
+
+  var tableArray = [];
+  var query = "SELECT * FROM products";
+  connection.query(query, function(err, rows){
+    for (i = 0; i < rows.length; i++) {
+      tableArray.push([rows[i].item_id, rows[i].product_name, rows[i].price]);
+    }
+
+    for (i= 0; i < rows.length; i++) {
+      table.push(tableArray[i]);
+    }
+
+    console.log(table.toString());
+
+    runSearch();
+  })
+};
+
+function runSearch() {
+  inquirer.prompt ([
+  {
+    name: 'id',
+    message: 'What is the product ID that you would like to buy?' 
+  },
+  {
+    name: 'quantity',
+    message: 'How many would you like to buy?'
+  }
+  ]).then(function(answers){
+    checkProduct(answers.id, answers.quantity);
+  });
+};
+
+
+function checkProduct(id, quantity) {
+  console.log("Checking availability...");
+
+  var query = "SELECT stock_quantity FROM products WHERE ?";
+
+  connection.query(query, {item_id: id}, function(err, res){
+
+    stock = res[0].stock_quantity;
+
+      if(res[0].stock_quantity >= quantity) {
+        console.log("We have availability! Purchasing now!");
+        buyProduct(id, quantity);
+      } else {
+        console.log("Insufficient quantity!");
+        connection.end();
+      };
+  });
+}; 
+  
+function buyProduct(id, quantity){
+  updatedQuantity = stock - quantity;
+
+  var query = "UPDATE products SET ? WHERE ?";
+
+    connection.query(query, [{stock_quantity: updatedQuantity}, {item_id: id}], function(err, res) {
+      console.log("Purchase is successful!");
+      showPrice(id, quantity);
+    }
+  );
+}
+
+function showPrice(id, quantity){
+  var query = "SELECT price FROM products WHERE ?";
+
+  connection.query(query, {item_id: id}, function(err, res){
+    console.log("Your total purchase was " + (res[0].price * quantity));
+    connection.end();
+  });
+};
+  
+
